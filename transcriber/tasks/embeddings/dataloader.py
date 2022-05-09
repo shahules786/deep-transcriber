90079555ec180abab64f9be6eea0404cd908ff54
shahules786 <shahules786@gmail.com>
2022-05-09 17:02:03 +0530

    TIMIT dataset

---
+++ transcriber/tasks/embeddings/dataloader.py
@@ -0,0 +1,48 @@
+from torch.utils.data import DataLoader,Dataset
+import numpy as np
+import glob 
+import os 
+
+
+
+class TimitDataset(Dataset):
+
+    def __init__(
+        self,
+        directory:str,
+        n_utterances:int
+    ):
+        self.directory = directory
+        self.n_utterances = n_utterances
+        self.utterances = self.filter_utterances()
+
+
+    def filter_utterances(
+        self,
+    ):
+        selected_files = []
+        np_files = [file for file in glob.glob(os.path.join(self.directory,"*")) if file.endswith("npy")]
+        for file in np_files:
+            if np.load(file).shape[0] >= self.n_utterances:
+                selected_files.append(file)
+
+        return selected_files
+
+
+    def __getitem__(
+        self, 
+        index
+    ):
+
+        npy_file = np.load(self.filter_utterances[index])
+        utter_start = np.random.randint(0,npy_file.shape[0] - self.n_utterances)
+        utterances = npy_file[utter_start:utter_start+self.n_utterances]
+        
+        return utterances
+
+    def __len__(
+        self,
+
+    ):
+        return len(self.filter_utterances)
+
