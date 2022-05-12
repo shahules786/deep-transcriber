dcc90538dc3c48da51c5d83735dbb7bf0cbb3808
shahules786 <shahules786@gmail.com>
2022-05-12 10:51:58 +0530

    prepare dataloaders

--- transcriber/tasks/embeddings/trainer.py
+++ transcriber/tasks/embeddings/trainer.py
@@ -1,7 +1,9 @@
 
 import os
 import torch
+from torch.utils.data import DataLoader
 
+from transcriber.tasks.embeddings.dataloader import TimitDataset,TimitCollate
 from transcriber.tasks.utils import min_value_check, path_check
 
 class EmbedTrainer:
@@ -10,8 +12,8 @@ class EmbedTrainer:
         self,
         train:str,
         test:str,
-        num_speakers:int,
-        num_utters:int,
+        n_speakers:int,
+        n_utterances:int,
         batch_size:int,
         epochs:int,
         lr:float,
@@ -27,20 +29,23 @@ class EmbedTrainer:
         if path_check(test):
             self.test = test
 
-        if min_value_check(num_speakers,0):
-            self.num_speakers = num_speakers
+        if min_value_check(n_speakers,0):
+            self.n_speakers = n_speakers
 
-        if min_value_check(num_utters,0):
-            self.num_layers = num_layers
+        if min_value_check(n_utterances,0):
+            self.n_utterances = n_utterances
 
         if min_value_check(batch_size,0):
-            self.num_layers = num_layers
+            self.batch_size = batch_size
 
         if min_value_check(epochs,0):
+            self.epochs = epochs
+        
+        if min_value_check(num_layers,0):
             self.num_layers = num_layers
 
         if min_value_check(hidden_layers,0):
-            self.num_layers = num_layers
+            self.hidden_layers = hidden_layers
 
         if min_value_check(embedding_dim,0):
             self.num_layers = num_layers
@@ -52,13 +57,29 @@ class EmbedTrainer:
                 self.device = torch.device(device)
             else:
                 raise ValueError(f"{device} not available!")
-        
+        self.lr = lr
     
     def train(
         self,
     ):
+        datalaoders = self._prepare_dataloaders()
         pass
+        
+    def _prepare_dataloaders(
+        self,
+    ):
+        train_dataset = TimitDataset(directory=self.train, n_utterances=self.n_utterances,
+                                        n_speakers = self.n_speakers)
+        collate_fn = TimitCollate(n_speakers = self.n_speakers, n_utterances=self.n_utterances)
+        train_dataset = DataLoader(train_dataset, batch_size=self.batch_size, shuffle=True, collate_fn=collate_fn)
 
+        valid_dataset = TimitDataset(directory=self.train, n_utterances=self.n_utterances,
+                                        n_speakers = self.n_speakers)
+        collate_fn = TimitCollate(n_speakers = self.n_speakers, n_utterances=self.n_utterances)
+        valid_dataset = DataLoader(valid_dataset, batch_size=self.batch_size, shuffle=True, collate_fn=collate_fn)
+
+        return {"train":train_dataset,
+                "valid":valid_dataset}
 
     def eval(
         self,
