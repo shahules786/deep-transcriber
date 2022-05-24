daed73f883fcfd16422580339f610704d88dff20
shahules786 <shahules786@gmail.com>
2022-05-24 13:18:53 +0530

    download data from kaggle

--- transcriber/tasks/utils.py
+++ transcriber/tasks/utils.py
@@ -1,5 +1,9 @@
 
+import logging
 import os
+import json
+import zipfile
+
 
 def min_value_check(arg,value):
     if arg<=value:
@@ -12,3 +16,21 @@ def path_check(path):
     if not os.path.exists(path):
         raise FileNotFoundError(f"File {path} does not exists")
     return True
+
+def download_data_kaggle(file_name='programmerrdai/house-price-to-the-moon',save_path:str="./data/"):
+
+    credentials = json.load(open("./data/kaggle.json","r"))
+    os.environ['KAGGLE_USERNAME'] = credentials["username"]
+    os.environ['KAGGLE_KEY'] = credentials["key"]
+    
+    from kaggle.api.kaggle_api_extended import KaggleApi
+    api = KaggleApi()
+    api.authenticate()
+    api.dataset_download_files(file_name, path=save_path)
+    logging.info("Data downloaded..")
+    with zipfile.ZipFile(os.path.join(save_path,f'{file_name.split("/")[-1]}.zip'), 'r') as zip_ref:
+        zip_ref.extractall(save_path)
+
+
+if __name__ == "__main__":
+    download_data_kaggle()
