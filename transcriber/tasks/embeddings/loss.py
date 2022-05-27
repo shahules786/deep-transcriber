29599f003119d1fc57864fa1575e2303a95d5fc6
shahules786 <shahules786@gmail.com>
2022-05-27 17:15:36 +0530

    loss

--- transcriber/tasks/embeddings/loss.py
+++ transcriber/tasks/embeddings/loss.py
@@ -13,8 +13,8 @@ class Ge2eLoss(nn.Module):
         super(Ge2eLoss,self).__init__()
         self.N = N
         self.M = M  
-        self.w = nn.parameter.Parameter(torch.tensor(5.0))
-        self.b = nn.parameter.Parameter(torch.tensor(1.0))
+        self.w = nn.parameter.Parameter(torch.tensor(10.0))
+        self.b = nn.parameter.Parameter(torch.tensor(-5.0))
 
     def forward(
         self,
@@ -22,17 +22,17 @@ class Ge2eLoss(nn.Module):
     ):
         centroids = embeddings.mean(dim=1)
         sum_ = embeddings.sum(dim=1)
-        cosine_sim = torch.mm(embeddings.reshape(self.N*self.M,-1),centroids.transpose(0,1))
-        for j in range(self.N):
-            for k in range(self.M):
-                c_j = sum_[j] - embeddings[j,k,:] / (1-self.M)
-                cosine_sim[self.M*j+k][j] = torch.dot(c_j,embeddings[j,k,:])
+        e = embeddings.view(self.N*self.M,-1)
+        cosine_sim = torch.mm(e,centroids.transpose(0,1))
         
+        for j in range(self.N):
+            for i in range(self.M):
+                cj = (sum_[j] - e[j*self.M + i]) / (self.M - 1)
+                cosine_sim[j*self.M + i][j] = torch.dot(cj, e[j*self.M + i])
         cosine_sim = self.w*cosine_sim + self.b 
         labels = torch.zeros((self.N*self.M,),dtype=int,device=embeddings.device)
         for i,j in enumerate(range(0,self.N*self.M,self.M)):
             labels[j:j+self.M] = i
-
         loss = cross_entropy(cosine_sim,labels)
         return loss
         
