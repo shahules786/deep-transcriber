5a6a4f90df3d5ac394ffd1be917ea7833748383f
shahules786 <shahules786@gmail.com>
2022-06-24 11:24:01 +0530

    fix input shapes

--- transcriber/tasks/segmentation/loss.py
+++ transcriber/tasks/segmentation/loss.py
@@ -7,11 +7,11 @@ PREDEFINED_LOSS = {"mse":nn.MSELoss(reduction="none"),
                     "bce":nn.BCELoss(reduction="none"),
                     }
 
-class PIT_Optimal:
+class PermutationInvarientTraining:
 
     def __init__(
         self,
-        loss:str="mse"
+        loss:str="bce"
     ):
         if PREDEFINED_LOSS.get(loss) is not None:
             self.loss = PREDEFINED_LOSS[loss]
@@ -31,27 +31,29 @@ class PIT_Optimal:
         if num_frames_y1!=num_frames_y2:
             raise ValueError(f"number of frames y1 {num_frames_y1} must be equal to number of frames y2 {num_frames_y2}")
         
-        permutated_y2 = torch.zeros(y1.shape, device=y2.device, dtype=y2.dtype)
+        permutated_y2 = torch.zeros(y1.shape, device=y2.device, dtype=y2.dtype,)
         permutations = []
         for b,(y1_,y2_) in enumerate(zip(y1,y2)):
-            cost_matrix = torch.stack(
-                [torch.mean(self.loss(y2_,y1_[:,i:i+1].expand(-1,num_classes_y2)),dim=0) 
-                    for i in range(num_classes_y1)]
-            )
+            with torch.no_grad():
+                cost_matrix = torch.stack(
+                    [torch.mean(self.loss(y2_,y1_[:,i:i+1].expand(-1,num_classes_y2)),dim=0) 
+                        for i in range(num_classes_y1)]
+                )
 
-            if num_classes_y1<num_classes_y2:
-                pad = (0,0,0,num_classes_y2-num_classes_y1)
-                cost_matrix = F.pad(
+        if num_classes_y1<num_classes_y2:
+            pad = (0,0,0,num_classes_y2-num_classes_y1)
+            cost_matrix = F.pad(
                     cost_matrix,
                     pad,
                     torch.max(cost_matrix)+1
-                )
-            permutation = [None] * num_classes_y1
-            for k1,k2 in zip(*linear_sum_assignment(cost_matrix)):
-                if k1<=num_classes_y1:
-                    permutated_y2[b,:,k1] = y2_[:,k2]
-                    permutation[k1] = k2
-                permutations.append(tuple(permutation))
+            )
+
+        permutation = [None] * num_classes_y1
+        for k1,k2 in zip(*linear_sum_assignment(cost_matrix.cpu())):
+            if k1<=num_classes_y1:
+                permutated_y2[b,:,k1] = y2_[:,k2]
+                permutation[k1] = k2
+            permutations.append(tuple(permutation))
 
         return permutated_y2, permutations
 
@@ -73,9 +75,9 @@ class losses:
     ):
 
         if self.loss == "bce":
-            return F.binary_cross_entropy(input,target)
+            return F.binary_cross_entropy(input,target.float())
         elif self.loss == "mse":
-            return F.mse_loss(input, target)
+            return F.mse_loss(input, target.float())
         else:
             raise ValueError("loss should be either 'bce' or 'mse'")
 
@@ -86,7 +88,7 @@ class losses:
     ):
         
         input,_ = torch.max(input,dim=2,keepdim=True)
-        target,_ = torch.max(target,dim=2,keepdim=False)
+        target,_ = torch.max(target,dim=2,keepdim=True)
 
         if self.loss == "bce":
             return F.binary_cross_entropy(input, target)
