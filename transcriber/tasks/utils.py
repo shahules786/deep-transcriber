920999d24adb7713d11048a03930801fa48e650f
shahules786 <shahules786@gmail.com>
2022-07-02 10:27:40 +0530

    early stopping

--- transcriber/tasks/utils.py
+++ transcriber/tasks/utils.py
@@ -69,3 +69,63 @@ def random_generation():
     rng.seed(global_seed*worker_id+num_workers)
     return rng
 
+
+class EarlyStopping:
+
+    def __init__(
+        self,
+        patience:int=10,
+        mode="min",
+        filename="model.pth"
+
+    ):
+        self.patience = patience 
+        self.counter = 0
+        if mode in ("min","max"):
+            self.mode = mode
+        else:
+            raise ValueError("mode must be either 'min' or 'max'")
+        self.best_loss = None
+        self.early_stop = False
+        self.filename = filename
+
+
+    def __call__(
+        self,
+        loss,
+        model
+
+    ):
+        if self.best_loss == None:
+            self.best_loss = loss
+        else:
+            if self.mode == "min":
+                if loss < self.best_loss:
+                    self.best_loss = loss
+                    self.save_checkpoint(model)
+                else:
+                    self.counter += 1
+            else:
+                if loss > self.best_loss:
+                    self.best_loss = loss
+                    self.save_checkpoint(model)
+                else:
+                    self.counter += 1
+
+
+        if self.counter >= self.patience:
+            print(f"Early stopping with best loss {self.best_loss}")
+            self.early_stop = True
+
+    def save_checkpoint(
+        self,
+        model
+    ):
+        torch.save(model.state_dict(),self.filename)
+
+
+
+
+
+    
+
